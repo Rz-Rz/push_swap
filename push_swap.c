19658cff40067089d9c243e98cdb3a265c0a0d6a
Karim Dhrif <karimdhrif4444@gmail.com>
2022-10-18 12:36:16 +0200

    working on quicksort

--- push_swap.c
+++ push_swap.c
@@ -16,30 +16,67 @@ node *fill_stack_a(int ac, char **av) {
   i = 2;
   stack_a->next = malloc(sizeof(node));
   curr = stack_a->next;
-  while (i < ac) {
+  while (av[i]) {
     curr->value = ft_atoi(av[i]);
+    i++;
+    if (!av[i])
+      break;
     curr->next = malloc(sizeof(node));
     curr = curr->next;
-    i++;
   }
   return (stack_a);
 }
 
-void print_stack_a(node *stack_a) {
+void put_pos(node *stack) {
   node *curr;
-  curr = stack_a;
+  int i;
+
+  curr = stack;
+  i = 1;
   while (curr) {
-    printf("%d", curr->value);
+    curr->index = i;
+    if (!curr->next)
+      break;
+    curr = curr->next;
+    i++;
+  }
+}
+
+void quicksort(node *stack) {}
+
+void put_index(node *stack) {
+  node *curr;
+  int i;
+
+  curr = stack;
+  i = 1;
+}
+
+void print_stack(node *stack) {
+  node *curr;
+  if (!stack) {
+    printf("stack is empty");
+    return;
+  }
+  curr = stack;
+  while (curr) {
+    printf("value : %d\n", curr->value);
+    printf("index : %d\n", curr->index);
     curr = curr->next;
   }
 }
 
 void push_swap(int ac, char **av) {
   node *stack_a;
+  node *stack_b;
   stack_a = fill_stack_a(ac, av);
+  put_pos(stack_a);
+  print_stack(stack_a);
 }
 
 int main(int ac, char **av) {
+  if (ac < 2)
+    return (0);
   push_swap(ac, av);
 
   return (0);
---
+++ sorting.c
@@ -0,0 +1,11 @@
+#include "push_swap.h"
+
+void quick_sort(int ac, node *stack) {
+  int i;
+  int pi;
+
+  i = 0;
+  if (stack[i] < stack[ac - 1]) {
+    pi = partition(stack, )
+  }
+}
