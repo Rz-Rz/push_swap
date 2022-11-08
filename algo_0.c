90df6737e05518b887cde4037e04ee8d34c3777e
Karim Dhrif <karimdhrif4444@gmail.com>
2022-11-08 22:02:38 +0100

    working version 1

--- algo_0.c
+++ algo_0.c
@@ -16,96 +16,97 @@
 // Description: Sort the stack_a with 2 elements
 // Input: node **stack_a - the stack to sort
 // Output: void
-void sort_2(node **stack_a) 
-{
+void sort_2(node **stack_a) {
   if ((*stack_a)->value > (*stack_a)->next->value)
     sa(stack_a);
 }
 
 // Function: sort_3
-// Description: Sort the stack_a with 3 elements using the index of the nodes. 
+// Description: Sort the stack_a with 3 elements using the index of the nodes.
 // Input: node **stack_a - the stack to sort
 // Output: void
-void sort_3(node **stack_a) 
-{
-	while (!is_ordered(*stack_a))
-	{
-		if ((*stack_a)->index > (*stack_a)->next->index && (*stack_a)->index > (*stack_a)->next->next->index)
-			ra(stack_a);
-		else if ((*stack_a)->index > (*stack_a)->next->index)
-			sa(stack_a);
-		else
-			rra(stack_a);
-	}
+void sort_3(node **stack_a) {
+  while (!is_ordered(*stack_a)) {
+    if ((*stack_a)->index > (*stack_a)->next->index &&
+        (*stack_a)->index > (*stack_a)->next->next->index)
+      ra(stack_a);
+    else if ((*stack_a)->index > (*stack_a)->next->index)
+      sa(stack_a);
+    else
+      rra(stack_a);
+  }
 }
 
-//Function: sort
-//Description: Sort the stack_a with n>3 elements using the index of the nodes. To decide of the most efficient action, we compare the numbers of action that will cost to put each element at the top of the pile. Then we choose the cheapest sequence. 
-// First we pb all the elements of stack_a except 3 elements. Then we sort the 3 elements with sort_3. Then we pa the elements of stack_b in the right order. As long as there are elements in stack_b we loop. First we find the actual position of all in stack_a and stack_b. Then we compute the target position (i.e. the index) of stack_a where each elements of stack_b should be. Then we numbers of action to put each element of stack_b to its target position in stack_a and we choose the cheapest option. Then we do the set of action needed to move the elements from stack_b to stack_a. 
-// Input: node **stack_a - the stack to sort to. node **stack_b - the stack to sort from.
-// Output: void
-void sort(node **stack_a, node **stack_b)
-{
-	node *to_sort;
+// Function: sort
+// Description: Sort the stack_a with n>3 elements using the index of the nodes.
+// To decide of the most efficient action, we compare the numbers of action that
+// will cost to put each element at the top of the pile. Then we choose the
+// cheapest sequence.
+//  First we pb all the elements of stack_a except 3 elements. Then we sort the
+//  3 elements with sort_3. Then we pa the elements of stack_b in the right
+//  order. As long as there are elements in stack_b we loop. First we find the
+//  actual position of all in stack_a and stack_b. Then we compute the target
+//  position (i.e. the index) of stack_a where each elements of stack_b should
+//  be. Then we numbers of action to put each element of stack_b to its target
+//  position in stack_a and we choose the cheapest option. Then we do the set of
+//  action needed to move the elements from stack_b to stack_a. Input: node
+//  **stack_a - the stack to sort to. node **stack_b - the stack to sort from.
+//  Output: void
+void sort(node **stack_a, node **stack_b) {
+  node *to_sort;
 
-	push_to_b(stack_a, stack_b);
-	while (*stack_b)
-	{
-		put_pos(*stack_a, *stack_b);
-		put_index(*stack_a, *stack_b);
-		put_target_pos(*stack_a, *stack_b);
-		put_costs(*stack_b, *stack_a);
-		to_sort = find_cheapest_action(*stack_b);
-		do_actions(stack_a, stack_b, to_sort);
-	}
-	rotate_until_sorted(stack_a);
+  push_to_b(stack_a, stack_b);
+  while (*stack_b) {
+    put_pos(*stack_a, *stack_b);
+    put_index(*stack_a, *stack_b);
+    put_target_pos(*stack_a, *stack_b);
+    put_costs(*stack_a, *stack_b);
+    to_sort = find_cheapest_action(*stack_b);
+    do_actions(stack_a, stack_b, to_sort);
+  }
+  rotate_until_sorted(stack_a);
 }
 
 // Function: push_to_b
-// Description: Push all the elements of stack_a to stack_b except 3 elements. Then we sort the 3 elements with sort_3.
-// Input: node **stack_a - the stack to push from.  node **stack_b - the stack to push to.
-// Output: void
-void push_to_b(node **stack_a, node **stack_b)
-{
-	int i;
-	int size;
+// Description: Push all the elements of stack_a to stack_b except 3 elements.
+// Then we sort the 3 elements with sort_3. Input: node **stack_a - the stack to
+// push from.  node **stack_b - the stack to push to. Output: void
+void push_to_b(node **stack_a, node **stack_b) {
+  int i;
+  int size;
 
-	size = lst_size(*stack_a);
-	i = 0;
-	while (i < size - 3)
-	{
-		pb(stack_a, stack_b);
-		i++;
-	}
-	put_index(*stack_a, *stack_b);
-	sort_3(stack_a);
-	put_target_pos(*stack_a, *stack_b);
+  size = lst_size(*stack_a);
+  i = 0;
+  while (i < size - 3) {
+    pb(stack_a, stack_b);
+    i++;
+  }
+  put_index(*stack_a, *stack_b);
+  sort_3(stack_a);
+  put_target_pos(*stack_a, *stack_b);
 }
 
 // Function: put_pos
-// Description: Put the position of each node in the linked list, starting from 0 to lst_size.
-// Input: node *stack_a - the linked list to put the position in.
+// Description: Put the position of each node in the linked list, starting from
+// 0 to lst_size. Input: node *stack_a - the linked list to put the position in.
 //        node *stack_b - the linked list to put the position in.
 // Output: void
-void put_pos(node *stack_a, node *stack_b)
-{
-	int i;
-	node *tmp;
+void put_pos(node *stack_a, node *stack_b) {
+  int i;
+  node *tmp;
 
-	i = 0;
-	tmp = stack_a;
-	while (tmp)
-	{
-		tmp->pos = i;
-		tmp = tmp->next;
-		i++;
-	}
-	i = 0;
-	tmp = stack_b;
-	while (tmp)
-	{
-		tmp->pos = i;
-		tmp = tmp->next;
-		i++;
-	}
+  i = 0;
+  tmp = stack_a;
+  while (tmp) {
+    tmp->pos = i;
+    tmp = tmp->next;
+    i++;
+  }
+  i = 0;
+  tmp = stack_b;
+  while (tmp) {
+    tmp->pos = i;
+    tmp = tmp->next;
+    i++;
+  }
 }
--- push_swap.c
+++ push_swap.c
@@ -12,12 +12,11 @@
 
 #include "push_swap.h"
 
-// Function: fill_stack_a 
-// Description: Take all the argument in av, transform them into int and put them in a linked list.
-// Input: int ac - the number of arguments, char **av - the arguments
-// Output: node * - the linked list
-node *fill_stack_a(char **av)
-{
+// Function: fill_stack_a
+// Description: Take all the argument in av, transform them into int and put
+// them in a linked list. Input: int ac - the number of arguments, char **av -
+// the arguments Output: node * - the linked list
+node *fill_stack_a(char **av) {
   node *stack_a;
   node *curr;
   int i;
@@ -39,10 +38,9 @@ node *fill_stack_a(char **av)
   }
   curr->next = NULL;
   error += is_duplicate(stack_a);
-  if (error)
-  {
-	  lst_free(stack_a);
-	  return (NULL);
+  if (error) {
+    lst_free(stack_a);
+    return (NULL);
   }
   return (stack_a);
 }
@@ -51,8 +49,7 @@ node *fill_stack_a(char **av)
 // Description: Print the values of the linked list
 // Input: node *stack - the linked list
 // Output: void
-void print_stack(node *stack) 
-{
+void print_stack(node *stack) {
   node *curr;
   if (!stack) {
     printf("stack is empty");
@@ -70,86 +67,85 @@ void print_stack(node *stack)
     curr = curr->next;
   }
 }
-void print_node(node *n) 
-{
+void print_node(node *n) {
   node *curr;
   if (!n) {
     printf("stack is empty");
     return;
   }
   curr = n;
-    printf("value : %d\n", curr->value);
-    printf("index : %d\n", curr->index);
-    printf("pos : %d\n", curr->pos);
-    printf("cost_a : %d\n", curr->cost_a);
-    printf("cost_b : %d\n", curr->cost_b);
-    printf("target pos : %d\n", curr->target_pos);
-    printf("--- node end ---\n");
+  printf("value : %d\n", curr->value);
+  printf("index : %d\n", curr->index);
+  printf("pos : %d\n", curr->pos);
+  printf("cost_a : %d\n", curr->cost_a);
+  printf("cost_b : %d\n", curr->cost_b);
+  printf("target pos : %d\n", curr->target_pos);
+  printf("--- node end ---\n");
 }
 
 int push_swap(int ac, char **av) {
-	node *stack_a;
-	node *stack_b;
-	stack_b = NULL;
-	stack_a = fill_stack_a(av);
-	if (stack_a == NULL)
-		return  (-1);
-	/* pb(&stack_a, &stack_b); */
-	/* put_pos(stack_a, stack_b); */
-	/* put_index(stack_a, stack_b); */
-	/* put_target_pos(stack_b, stack_a); */
-	/* put_costs(stack_a, stack_b); */
-	/* sort_3(&stack_a); */
-	/* printf("stack_a\n"); */
-	/* print_stack(stack_a); */
-	/* put_pos(stack_a, stack_b); */
-	/* put_index(stack_a, stack_b); */
-	/* put_target_pos(stack_b, stack_a); */
-	/* put_costs(stack_a, stack_b); */
-	/* printf("---stack_b---\n"); */
-	/* print_stack(stack_b); */
-	/* printf("CHEAPEST NODE\n"); */
-	/* print_node(find_cheapest_action(stack_b)); */
-	/* printf("stack_a after sort\n"); */
-	/* print_stack(stack_a); */
-	/* do_actions(&stack_a, &stack_b, find_cheapest_action(stack_b)); */
-	/* printf("stack_a after action\n"); */
-	/* print_stack(stack_a); */
-	/* node *to_sort; */
-	/* push_to_b(&stack_a, &stack_b); */
-	/* put_pos(stack_a, stack_b); */
-	/* put_index(stack_a, stack_b); */
-	/* put_target_pos(stack_a, stack_b); */
-	/* put_costs(stack_a, stack_b); */
-	/* to_sort = find_cheapest_action(stack_b); */
-	/* printf("to_sort\n"); */
-	/* print_node(to_sort); */
-	/* do_actions(&stack_a, &stack_b, to_sort); */
-	/* printf("stack_a\n"); */
-	/* print_stack(stack_a); */
-	/* printf("stack_b\n"); */
-	/* print_stack(stack_b); */
-	if (ac == 3)
-		sort_2(&stack_a);
-	else if (ac == 4)
-		sort_3(&stack_a);
-	else if ( ac > 4 && ac < 501)
-		sort(&stack_a, &stack_b);
-	/* printf("stack_a\n"); */
-	/* print_stack(stack_a); */
-	/* printf("stack_b\n"); */
-	/* print_stack(stack_b); */
+  node *stack_a;
+  node *stack_b;
+  stack_b = NULL;
+  stack_a = fill_stack_a(av);
+  if (stack_a == NULL)
+    return (-1);
+  /* pb(&stack_a, &stack_b); */
+  /* put_pos(stack_a, stack_b); */
+  /* put_index(stack_a, stack_b); */
+  /* put_target_pos(stack_b, stack_a); */
+  /* put_costs(stack_a, stack_b); */
+  /* sort_3(&stack_a); */
+  /* printf("stack_a\n"); */
+  /* print_stack(stack_a); */
+  /* put_pos(stack_a, stack_b); */
+  /* put_index(stack_a, stack_b); */
+  /* put_target_pos(stack_b, stack_a); */
+  /* put_costs(stack_a, stack_b); */
+  /* printf("---stack_b---\n"); */
+  /* print_stack(stack_b); */
+  /* printf("CHEAPEST NODE\n"); */
+  /* print_node(find_cheapest_action(stack_b)); */
+  /* printf("stack_a after sort\n"); */
+  /* print_stack(stack_a); */
+  /* do_actions(&stack_a, &stack_b, find_cheapest_action(stack_b)); */
+  /* printf("stack_a after action\n"); */
+  /* print_stack(stack_a); */
+  /* node *to_sort; */
+  /* push_to_b(&stack_a, &stack_b); */
+  /* put_pos(stack_a, stack_b); */
+  /* put_index(stack_a, stack_b); */
+  /* put_target_pos(stack_a, stack_b); */
+  /* put_costs(stack_a, stack_b); */
+  /* to_sort = find_cheapest_action(stack_b); */
+  /* printf("to_sort\n"); */
+  /* print_node(to_sort); */
+  /* do_actions(&stack_a, &stack_b, to_sort); */
+  /* printf("stack_a\n"); */
+  /* print_stack(stack_a); */
+  /* printf("stack_b\n"); */
+  /* print_stack(stack_b); */
+  if (ac == 3)
+    sort_2(&stack_a);
+  else if (ac == 4)
+    sort_3(&stack_a);
+  else if (ac > 4 && ac < 501)
+    sort(&stack_a, &stack_b);
+  printf("stack_a\n");
+  print_stack(stack_a);
+  /* printf("stack_b\n"); */
+  /* print_stack(stack_b); */
 
-	return (1);
+  return (1);
 }
 
 int main(int ac, char **av) {
-	int i = 0;
-	if (ac < 2)
-		return (0);
-	i = push_swap(ac, av);
-	if (i == -1)
-		printf("Error\n");
+  int i = 0;
+  if (ac < 2)
+    return (0);
+  i = push_swap(ac, av);
+  if (i == -1)
+    printf("Error\n");
 
-	return (0);
+  return (0);
 }
